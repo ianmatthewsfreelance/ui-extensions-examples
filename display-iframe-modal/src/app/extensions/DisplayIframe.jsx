aa25bd05e308e8298fbfce1ff30029fb774993f8
ianmatthewsfreelance <148258591+ianmatthewsfreelance@users.noreply.github.com>
2024-07-02 17:04:23 +0100

    Update DisplayIframe.jsx

--- display-iframe-modal/src/app/extensions/DisplayIframe.jsx
+++ display-iframe-modal/src/app/extensions/DisplayIframe.jsx
@@ -9,7 +9,7 @@ hubspot.extend((
 const Extension = ({ openIframe }) => {
   const handleClick = () => {
     openIframe({
-      uri: "https://wikipedia.com/", // this is a relative link. Some links will be blocked since they don't allow iframing
+      uri: "https://www.freelancehubspotexpert.com/", // this is a relative link. Some links will be blocked since they don't allow iframing
       height: 1000,
       width: 1000,
       title: 'Wikipedia in an Iframe',
@@ -21,11 +21,7 @@ const Extension = ({ openIframe }) => {
     <>
       <Flex direction="column" align="start" gap="medium">
         <Text>
-          Clicking the button will open a modal dialog with an iframe that
-          displays the content at the provided URL. Get more info on how to do this {" "}.
-          <Link href="https://developers.hubspot.com/docs/platform/create-ui-extensions#open-an-iframe-in-a-modal">
-            here
-          </Link>
+          Reference my website below if needed for anything.
         </Text>
 
         <Box>
